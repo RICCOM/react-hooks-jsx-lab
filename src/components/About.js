d583ed93f9458f4ca7999c006597502b09ad9788
RICCOM <ericomun@gmail.comgit>
2024-04-27 22:50:57 +0300

    Adding JSX elements

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about"><h2>About Me</h2><p>I try till I win</p><img src={image} alt= "I made this"/></div>;
 }
 
 export default About;
